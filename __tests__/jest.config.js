a46b5b7bdd9eb2f24dcbd5c8b0e26214b72139ee
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-09-25 21:01:48 -0300

    Adiciona pequena correçao na config do Jest

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
-const root = resolve(__dirname);
+const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
 module.exports = {
