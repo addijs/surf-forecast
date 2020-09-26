d7bc9c9f2ad1f25ee980090922b80d2b7907d732
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-09-25 23:14:36 -0300

    Adiciona setup do app Express + primeiro controller

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,29 @@
-import { Server } from '@overnightjs/core';
 import './utils/module-alias';
-import express from 'express';
-import init from 'module-alias';
+import { Server } from '@overnightjs/core';
+import express, { Application } from 'express';
+import { ForecastController } from './controllers/ForecastController';
 
 export class ServerSetup extends Server {
   constructor(private port = 3333) {
     super();
-
-    init();
   }
 
-  public init(): void {
+  public getApp(): Application {
+    return this.app;
+  }
+
+  public async init(): Promise<void> {
     this.initializeMiddlewares();
+    this.initializeControllers();
   }
 
   private initializeMiddlewares(): void {
     this.app.use(express.json());
   }
 
-  private initializeControllers() {}
+  private initializeControllers(): void {
+    const forecastController = new ForecastController();
+
+    this.addControllers([forecastController]);
+  }
 }
