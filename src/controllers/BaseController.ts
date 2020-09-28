c8796d6c2bebeab94deb721282b22b79a6e7343a
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-09-27 22:30:38 -0300

    Adiciona BaseController para metodos de erro

---
+++ src/controllers/BaseController.ts
@@ -0,0 +1,36 @@
+import { CUSTOM_VALIDATION } from '@src/models/user';
+import { Response } from 'express';
+import mongoose from 'mongoose';
+
+interface ErrorObject {
+  code: number;
+  error: string;
+}
+
+export abstract class BaseController {
+  protected sendCreateUpdateErrorResponse(
+    res: Response,
+    error: mongoose.Error.ValidationError | Error
+  ): void {
+    if (error instanceof mongoose.Error.ValidationError) {
+      const clientErrors = this.handleClientErrors(error);
+      res.status(clientErrors.code).send(clientErrors);
+    } else {
+      res.status(500).send({ code: 500, error: 'Internal Server Error' });
+    }
+  }
+
+  private handleClientErrors(
+    error: mongoose.Error.ValidationError
+  ): ErrorObject {
+    const duplicatedKindErrors = Object.values(error.errors).filter(
+      err => err.kind === CUSTOM_VALIDATION.DUPLICATED
+    );
+
+    if (duplicatedKindErrors.length) {
+      return { code: 409, error: error.message };
+    } else {
+      return { code: 422, error: error.message };
+    }
+  }
+}
