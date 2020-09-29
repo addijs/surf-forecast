7d8555077d998e228f26225f0051058650b110aa
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-09-28 21:03:06 -0300

    Adiciona graceful shutdown do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,50 @@
 import config from 'config';
 import { AppSetup } from './app';
+import logger from './logger';
+
+enum ExitStatus {
+  Failure = 1,
+  Success = 0,
+}
+
+// Handling with unhandle promises
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(
+    `App exiting due to an unhandled promise: ${promise} and reason ${reason}`
+  );
+  throw reason;
+});
+
+// Handling with uncaught exceptions
+
+process.on('uncaughtException', error => {
+  logger.error(`App exiting due to an uncaught exception: ${error}`);
+  process.exit(ExitStatus.Failure);
+});
 
 (async (): Promise<void> => {
-  const app = new AppSetup(config.get('App.port'));
-  await app.init();
+  try {
+    const app = new AppSetup(config.get('App.port'));
+    await app.init();
 
-  app.startApp();
+    app.startApp();
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    exitSignals.forEach(sig =>
+      process.on(sig, async () => {
+        try {
+          await app.closeApp();
+          logger.info('App exited with success');
+          process.exit(ExitStatus.Success);
+        } catch (err) {
+          logger.error(`App exited with error: ${err}`);
+          process.exit(ExitStatus.Failure);
+        }
+      })
+    );
+  } catch (err) {
+    logger.error(`App exited with error: ${err}`);
+    process.exit(ExitStatus.Failure);
+  }
 })();
